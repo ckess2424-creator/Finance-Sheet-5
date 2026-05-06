d1177f4b30fa9297257d68b88cd5ba04432d6139
ckess2424-creator <ckess2424@gmail.com>
2026-05-06 17:18:18 +0300

    Create Header.jsx

---
+++ Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <div className="header">
+      <h1>💰 Finance Sheet 5</h1>
+      <p>Track US + Israel accounts in real time</p>
+    </div>
+  );
+}
